9fd9371b9e340ca40fa15918eb5ef1476e9f7151
Catalin <uzy_electro@yahoo.com>
2022-07-20 11:59:46 +0300

    first commit

---
+++ tasks/src/app/simple-button/simple-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'simple-button',
+    templateUrl: './simple-button.component.html',
+    styleUrls: ['./simple-button.component.css']
+})
+export class SimpleButtonComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit(): void {
+    }
+
+}
