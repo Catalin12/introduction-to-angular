11b64cae71ea89c17058097e11007d0bc85d9e29
Catalin <uzy_electro@yahoo.com>
2022-07-22 11:36:18 +0300

    added the functionalities of the task

--- component-interaction/src/app/user-details/user-details.component.ts
+++ component-interaction/src/app/user-details/user-details.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { UserModel } from '../user.model';
 
 @Component({
 	selector: 'user-details',
@@ -6,5 +8,6 @@ import { Component } from '@angular/core';
 	styleUrls: ['./user-details.component.css']
 })
 export class UserDetailsComponent {
-
+	@Input()
+	public user?: UserModel;
 }
